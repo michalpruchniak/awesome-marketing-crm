f9198c013cb15c8c46f2ff66242db04696cd906d
Michal <goldenofheart@gmail.com>
2024-10-07 14:26:48 +0200

    Remove unnecessary files

--- Modules/Sites/app/Http/Controllers/SitesController.php
+++ Modules/Sites/app/Http/Controllers/SitesController.php
@@ -4,7 +4,6 @@ namespace Modules\Sites\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Modules\Customers\Services\CustomersService;
 use Modules\Sites\Services\SitesService;
 
 class SitesController extends Controller
